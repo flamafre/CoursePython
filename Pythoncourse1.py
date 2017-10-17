e3dd242b4af1d7be5c8f70ea10851aa97740df99
flamafre <flamafre@gmail.com>
2017-10-17 09:58:49 +0200

    Secondclass test

---
+++ Pythoncourse1.py
@@ -0,0 +1,27 @@
+def mymax(x,y):
+    """
+    returns the max of two numbers
+    input parameters: x, y (numbers)
+    
+    >>> mymax(3,5)
+    5
+    """
+    if x>y:
+        return x
+    else:
+        return y
+    
+def Listmax(l):
+    lmax = 0
+    for i in l:
+        if i> lmax:
+            lmax = 1
+    return lmax
+
+if __name__ == "__main__":
+    import sys
+    myList =[]
+    for arg in sys.argv[1:]:
+        myList.append(int(arg))
+    print(listmax(myList))
+    
